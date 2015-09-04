83b5fbb14da04be9e25e826e873efb9c1226a8bc
Stephen Deddens <sdeddens@gmail.com>
2015-09-04 10:02:35 -0500

    first js

---
+++ calc.js
@@ -0,0 +1,29 @@
+// calculations for jsCalc exercise GA WDI-02-w02d05  Deddens Pineda
+
+// arrange calculation functions in a calc object
+// start off with two functions: add and subtract
+// need to validate input data also
+
+
+// Main calculation function object:
+var calc {
+
+	function add (x,y){
+		return (x+y);
+	},
+
+	function subtract (x,y){
+		return (x-y);
+	}
+
+	// Add more functions here
+}
+
+function validate (userString){
+	// change sting to number
+	
+	parseInit (userString);
+	// do validation
+
+	// return false if not valid or return number if valid
+}
