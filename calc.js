620c66393d27656e43b78a8be72ed5c242488cee
Stephen Deddens <sdeddens@gmail.com>
2015-09-04 15:00:01 -0500

    uddate

--- calc.js
+++ calc.js
@@ -8,34 +8,16 @@
 // Main calculation function object:
 
 $(function(){
-	// var calc = {
-
-	// 	function add (x,y){
-	// 		return (x+y);
-	// 	},
-
-	// 	function subtract (x,y){
-	// 		return (x-y);
-	// 	}
-
-	// 	// Add more functions here
-	// }
-
-	// function validate (userString){
-	// 	// change sting to number
-		
-	// 	parseInit (userString);
-	// 	// do validation
-
-	// 	// return false if not valid or return number if valid
-	// }
-
+	
 	// Add event listeners
 	var strNum, numNum;
 	var outString = "";
 	var result = 0;
 	var state = 'add';
+	// get the input field jQuery object
+	var input = $("#input");
 
+	input.focus();
 	function updateResult () {
 		switch (state) {
 				
@@ -47,13 +29,22 @@ $(function(){
 				result -= numNum;
 				break;
 
-			case 'div':
+			case 'divi':
 				result /= numNum;
+				break;
 
+			case "mult":
+				result *= numNum;
+				break;
 		}
-
 	}
-	
+
+// number keys handlers	
+	$('#a1').click(function(){
+		input.val(input.val() + "1" );
+		$('#input').focus();
+	});
+
 	$('#add').click(function(){
 		// grab number from input box
 		strNum = $('#input').val();
@@ -75,7 +66,7 @@ $(function(){
 			$('#input').focus();
 	});
 
-	$('#-').click(function(){
+	$('#minus').click(function(){
 		// grab number from input box
 		strNum = $('#input').val();
 		numNum = parseInt (strNum);
@@ -96,7 +87,7 @@ $(function(){
 			$('#input').focus();
 	});
 
-	$('#div').click(function(){
+	$('#divi').click(function(){
 		// grab number from input box
 		strNum = $('#input').val();
 		numNum = parseInt (strNum);
@@ -104,12 +95,33 @@ $(function(){
 		if ((typeof numNum === 'number') && (numNum === numNum)){
 			// make our output string
 			updateResult();
-			outString = outString + String(numNum) + " / ";
+			outString = "("+outString + String(numNum) + ") / ";
 			// put result in box
 			$('#output').val(outString)
 			// clear the input box;
 			$('#input').val("");
-			state = 'div';
+			state = 'divi';
+
+		} else { 
+			alert('Please enter a valid numNum.');
+		};
+			$('#input').focus();
+	});
+
+	$('#mult').click(function(){
+		// grab number from input box
+		strNum = $('#input').val();
+		numNum = parseInt (strNum);
+		// valadate data for number and NaN 
+		if ((typeof numNum === 'number') && (numNum === numNum)){
+			// make our output string
+			updateResult();
+			outString = "("+ outString + String(numNum) + ") * ";
+			// put result in box
+			$('#output').val(outString)
+			// clear the input box;
+			$('#input').val("");
+			state = 'mult';
 
 		} else { 
 			alert('Please enter a valid numNum.');
